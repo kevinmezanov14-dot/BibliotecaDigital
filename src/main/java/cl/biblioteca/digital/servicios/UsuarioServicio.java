1588653759eafbe79cf6d8635630e8fb4b7f7dd2
Kevin Meza Catril <kevin.meza.nov14@gmail.com>
2026-03-09 19:28:57 -0300

    mejora en seguridad con BCrypt

--- src/main/java/cl/biblioteca/digital/servicios/UsuarioServicio.java
+++ src/main/java/cl/biblioteca/digital/servicios/UsuarioServicio.java
@@ -3,22 +3,24 @@ package cl.biblioteca.digital.servicios;
 import cl.biblioteca.digital.daos.UsuarioDAO;
 import cl.biblioteca.digital.daos.UsuarioDAOImpl;
 import cl.biblioteca.digital.model.Usuario;
+import org.mindrot.jbcrypt.BCrypt;
 import java.sql.SQLException;
 
 public class UsuarioServicio {
 
     private final UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
 
-    // Login por email
     public Usuario loginPorEmail(String email, String password) throws SQLException {
         Usuario usuario = usuarioDAO.obtenerPorEmail(email);
-        if (usuario != null && usuario.getPassword().equals(password) && usuario.isActivo()) {
+        if (usuario != null && BCrypt.checkpw(password, usuario.getPassword()) && usuario.isActivo()) {
             return usuario;
         }
         return null;
     }
-
     public int registrar(Usuario usuario) throws SQLException {
+        // Hashear contraseña antes de guardar
+        String hash = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt()); //genera unca cadena aleatoria llamada salt y la combina con la contraseña creada
+        usuario.setPassword(hash); //reemplaza la contraseña con el hash generado
         return usuarioDAO.insertar(usuario);
     }
 
